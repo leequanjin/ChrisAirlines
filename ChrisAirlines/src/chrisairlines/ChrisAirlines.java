1b2a40c31daecd223ffba99f7d7af13c774aa793
Eddead <simhk-wp22@student.tarc.edu.my>
2024-04-08 12:34:47 +0800

    Test Commit

--- ChrisAirlines/src/chrisairlines/ChrisAirlines.java
+++ ChrisAirlines/src/chrisairlines/ChrisAirlines.java
@@ -4,6 +4,7 @@ public class ChrisAirlines {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("PUSH TEST");
+        System.out.println("Eddie was here");
     }
     
 }
