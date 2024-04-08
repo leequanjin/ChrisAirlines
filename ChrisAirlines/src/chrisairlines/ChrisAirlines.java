c2441a2230ce6a6fef93421620a90262b711c930
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-04-08 11:55:28 +0800

    Test Push

--- ChrisAirlines/src/chrisairlines/ChrisAirlines.java
+++ ChrisAirlines/src/chrisairlines/ChrisAirlines.java
@@ -3,6 +3,7 @@ package chrisairlines;
 public class ChrisAirlines {
     public static void main(String[] args) {
         System.out.println("TEST");
+        System.out.println("PUSH TEST");
     }
     
 }
