57fd90100b9637d5fd82764f1b1230fd1be6b2b9
Eddead <simhk-wp22@student.tarc.edu.my>
2024-04-08 16:04:57 +0800

    Eh Test

--- ChrisAirlines/src/chrisairlines/ChrisAirlines.java
+++ ChrisAirlines/src/chrisairlines/ChrisAirlines.java
@@ -1,10 +1,22 @@
 package chrisairlines;
 
 public class ChrisAirlines {
-    public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("PUSH TEST");
-        System.out.println("Eddie was here");
+
+  public static void main(String[] args) {
+    Membership newMember = MembershipCreation.createMembership();
+
+    if (newMember != null) {
+      System.out.println("New member created successfully!");
+      // You can now access and potentially save the newMember object
+    } else {
+      System.out.println("Failed to create new membership.");
     }
     
+    System.out.println("\nYour UID: " + newMember.getMembership_id());
+    System.out.println("Your Name : " + newMember.getMember_name());
+    System.out.println("Your Contact Info (Phone) : " + newMember.getContact_info());
+    System.out.println("Your Membership Level : " + newMember.getMembership_level());
+    System.out.println("Your Points Accumulated : " + newMember.getPoints_accumulated());
+    System.out.println("Your Membership Status : " + newMember.getMembership_status());
+  }
 }
