f57506b5430734062b37d6b4d18c3420c088052d
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-04-13 16:47:51 +0800

    Test Push

--- ChrisAirlines/src/chrisairlines/ChrisAirlines.java
+++ ChrisAirlines/src/chrisairlines/ChrisAirlines.java
@@ -1,9 +1,29 @@
 package chrisairlines;
 
+import chrisairlines.Customer;
+import java.util.Scanner;
+
 public class ChrisAirlines {
     public static void main(String[] args) {
-        System.out.println("TEST");
-        System.out.println("PUSH TEST");
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        boolean validChoice = false;
+        
+        while (!validChoice) {
+            System.out.println("You wanna login or signup? \n1. Login \n2. Signup");
+            choice = scan.nextInt();
+
+            switch (choice) {
+                case 1 -> {
+                    Customer.login();
+                    validChoice = true;
+                }
+                case 2 -> {
+                    Customer.signUp();
+                    validChoice = true;
+                }
+                default -> System.out.println("Invalid choice. Please enter 1 for login or 2 for signup.");
+            }
+        }
     }
-    
 }
